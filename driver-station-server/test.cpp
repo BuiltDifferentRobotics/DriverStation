63627397307631bf3c9ca1196a88e2e70c4bc214
Zhiheng Hua <hua13@illinois.edu>
2021-10-13 19:18:41 -0500

    startup

---
+++ driver-station-server/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+int main()
+{
+    std::queue<int> q;
+    q.push(0);
+    q.push(1);
+    std::string str = "hey";
+    std::cout << 12345 << std::endl;
+}
