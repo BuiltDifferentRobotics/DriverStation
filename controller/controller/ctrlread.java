a91734ed00679e3e2160d0259c3324f79a90e6f3
Explicious <46688743+Explicious@users.noreply.github.com>
2021-10-20 19:48:52 -0500

    added ctrlread.java which creates a test pop-up

---
+++ controller/controller/ctrlread.java
@@ -0,0 +1,9 @@
+package controller;
+
+import de.hardcode.jxinput.test.JXInputTestDialog;
+
+public class ctrlread {
+	public static void main(String[] args) {
+		JXInputTestDialog.main(args);
+	}
+}
