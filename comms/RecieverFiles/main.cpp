a70873fcd3982d1f9958f9a01afe5019428b7689
Baguette2718 <aed6@illinois.edu>
2021-10-20 19:55:52 -0500

    fixed main
    json serializer in main works

--- comms/RecieverFiles/main.cpp
+++ comms/RecieverFiles/main.cpp
@@ -1,22 +1,35 @@
-//#include "nlohmann/json.hpp"
+#include "nlohmann/json.hpp"
 #include <string>
 #include <iostream>
-//using json = nlohmann::json;
+using json = nlohmann::json;
 
 struct Example {
     public:
+        std::string l;
         int n;
-        Example(int b) {
+        std::vector<int> v;
+        Example() {}
+        Example(std::string a, int b, std::vector<int> c) {
         n = b;
+        l = a;
+        v = c;
     }
 };
+void to_json(json& j, const Example& p) {
+    j = json{{"l", p.l}, {"n", p.n}, {"v", p.v}};
+}
 
+void from_json(const json& j, Example& p) {
+    j.at("l").get_to(p.l);
+    j.at("n").get_to(p.n);
+    j.at("v").get_to(p.v);
+}
 int main() {
-    Example ex(3);
-    // json j;
-    // j["s"] = ex.s;
-    // j["n"] = ex.n;
-    // //auto ex2 = j.get<Example>();
-    std::cout << "ee\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" << std::endl;
+    Example ex("hoHohoHo", 3, {1, 2, 3});
+    json j;
+    to_json(j, ex);
+    Example ex2;
+    from_json(j, ex2);
+    std::cout << ex2.l << std::endl;
     return 0;
 }
