3fc524d00375a3422cadc834a3e10bec09cd9030
Baguette2718 <aed6@illinois.edu>
2021-10-20 19:26:55 -0500

    Trying to get main to work

--- comms/RecieverFiles/main.cpp
+++ comms/RecieverFiles/main.cpp
@@ -1,23 +1,22 @@
-#include "nlohmann/json.hpp"
+//#include "nlohmann/json.hpp"
 #include <string>
-#include <vector>
-using json = nlohmann::json;
+#include <iostream>
+//using json = nlohmann::json;
 
 struct Example {
     public:
-        std::string s;
         int n;
-        std::vector<int> ni;
-        Example(std::string a, int b, std::vector<int> c) {
-        s = a;
+        Example(int b) {
         n = b;
-        ni = c;
     }
-}
+};
 
 int main() {
-    Example ex("re", 3, {3, 1, 4, 1, 5, 9, 2, 6});
-    json j = ex;
-    auto ex2 = j.get<Example>();
+    Example ex(3);
+    // json j;
+    // j["s"] = ex.s;
+    // j["n"] = ex.n;
+    // //auto ex2 = j.get<Example>();
+    std::cout << "ee\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" << std::endl;
     return 0;
 }
