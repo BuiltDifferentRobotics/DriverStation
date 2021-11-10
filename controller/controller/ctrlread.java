9de13b392281123715c782b659f349e804fc480e
Explicious <46688743+Explicious@users.noreply.github.com>
2021-11-10 11:26:36 -0600

    Update ctrlread.java

--- controller/controller/ctrlread.java
+++ controller/controller/ctrlread.java
@@ -1,9 +1,12 @@
 package controller;
 
 import de.hardcode.jxinput.test.JXInputTestDialog;
+import de.hardcode.jxinput.test.ButtonListener;
+import de.hardcode.jxinput.Button;
 
 public class ctrlread {
 	public static void main(String[] args) {
-		JXInputTestDialog.main(args);
+		Button testButton = new Button();
+		ButtonListener testListener = new ButtonListener(testButton);
 	}
 }
